a6dd79df6272ef89c3b50ccad7f946bbe997e855
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-03 22:23:38 -0300

    Add react to action item clicks on the Menu

--- app/src/main/java/com/pfariasmunoz/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/pfariasmunoz/bitsandpizzas/MainActivity.java
@@ -3,6 +3,7 @@ package com.pfariasmunoz.bitsandpizzas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +18,19 @@ public class MainActivity extends Activity {
         // inflate the menu: this adds items to the activity bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                // to run when the Create order item is clicked
+                return true;
+            case R.id.action_settings:
+                // to run when the settings item is clicked
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
