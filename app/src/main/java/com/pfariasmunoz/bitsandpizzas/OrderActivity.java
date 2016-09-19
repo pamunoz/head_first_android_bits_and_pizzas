78642fa545dcb2400964fbc8a914e8795e0bcd5e
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-19 17:49:59 -0300

    Add the Up button

--- app/src/main/java/com/pfariasmunoz/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/pfariasmunoz/bitsandpizzas/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.pfariasmunoz.bitsandpizzas;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,5 +11,15 @@ public class OrderActivity extends Activity {
         super.onCreate(savedInstanceState);
         setTitle(R.string.create_order);
         setContentView(R.layout.activity_order);
+        /**
+         * WATCH IT!
+         * if you enable the Up button for an activity, you must specify its parent in the Manifest.
+         * if you don't, you'll get a null pointer exception when you call the setDisplayHomeAsUpEnabled() method.
+         */
+        ActionBar actionBar = getActionBar();
+        // this enables the Up button in the action bar.
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+
 }
